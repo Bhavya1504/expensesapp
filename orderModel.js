9d9e60be66e7abc7dd19e24e9d3bba4b68e07ef7
Bhavya1504 <124488278+Bhavya1504@users.noreply.github.com>
2023-08-03 20:13:43 +0530

    Add files via upload

---
+++ orderModel.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../util/db');
+
+
+const Order = sequelize.define('order', {
+  // Model attributes are defined here
+  order_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  payment_id: {
+    type: DataTypes.STRING,
+  },
+  payment_sign: {
+    type: DataTypes.STRING,
+  },
+  payment_status:DataTypes.STRING,
+  
+}, {
+  // Other model options go here
+});
+
+module.exports=Order;
